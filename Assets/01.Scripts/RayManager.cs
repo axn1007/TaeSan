06c3fdbf906e98975d8b13781d7540b449865aff
SSo99r <ddvlep@gmail.com>
2021-05-08 01:55:06 +0900

    .

--- Assets/01.Scripts/RayManager.cs
+++ Assets/01.Scripts/RayManager.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class RayManager : MonoBehaviour
 {
-    AIMove aiMove;
+    public RaycastHit[] hits;
 
     void Start()
     {
-        aiMove = GameObject.Find("AI").GetComponent<AIMove>();
+
     }
 
     void Update()
     {
         int layer = 1 << LayerMask.NameToLayer("Event");
-        RaycastHit[] hits;
         hits = Physics.RaycastAll(
             Camera.main.transform.position,
             Camera.main.transform.forward,
             25, layer);
-        if (hits.Length == 2)
-        {
-            if (aiMove.wpIndex < aiMove.wayPointBox.Length - 1) aiMove.wpIndex++;
-            Destroy(hits[0].transform.gameObject);
-            //print("2");
-        }
-        else
-        {
-            //print("0");
-        }
+        //if (hits.Length == 2)
+        //{
+        //    print("2");
+        //}
+        //else
+        //{
+        //    print("0");
+        //}
         //foreach (RaycastHit hit in hits)
         //{
         //    Debug.Log("Raycast!");
