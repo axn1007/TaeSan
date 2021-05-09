c0a1df426e2c5adb3da7dfa74564d019c0db2a67
SSo99r <ddvlep@gmail.com>
2021-05-09 12:41:40 +0900

    .

---
+++ Assets/01.Scripts/TitleText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleText : MonoBehaviour
+{
+    public Text titleText;
+    public bool standby;
+    float alpha = 0;
+
+    void Update()
+    {
+        if (standby == true)
+        {
+            alpha += 0.2f * Time.deltaTime;
+            titleText.color = new Color(0.0f, 0.0f, 0.0f, alpha);
+        }
+    }
+}
