0f142b6ad744706e184e8b31e9a29dd5e28b3939
SSo99r <ddvlep@gmail.com>
2021-05-11 15:58:58 +0900

    .

--- Assets/KSY/Scripts2/BalloonMove.cs
+++ Assets/KSY/Scripts2/BalloonMove.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class BalloonMove : MonoBehaviour
 {
-    GameObject Balloon;
     Vector3 startPos;
-    public Vector3 destPos = new Vector3(15, 15, -3);
+    public Vector3 destPos;
     public float speed = 0.005f;
 
     void Start()
     {
-        startPos = Balloon.transform.position;
-        destPos = this.gameObject.transform.position;
+        startPos = new Vector3(-15, 16, 8);
+        destpos = new vector3(14, 17.5, -14);
     }
         
 
