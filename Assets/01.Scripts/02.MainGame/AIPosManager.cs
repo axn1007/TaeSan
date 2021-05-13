b7dfc5fe7ac30c6aea25c9de639e43d66aa61736
SSo99r <ddvlep@gmail.com>
2021-05-14 00:19:49 +0900

    .

--- Assets/01.Scripts/02.MainGame/AIPosManager.cs
+++ Assets/01.Scripts/02.MainGame/AIPosManager.cs
@@ -6,16 +6,27 @@ public class AIPosManager : MonoBehaviour
 {
     public GameObject[] aiPoss;
     public GameObject[] rayTargets;
+    public GameObject moveDummy;
+    public GameObject stageClearDummy;
+    public Transform moveDestPos;
     public Transform overbridgeDestPos;
     RayManager rayManager;
+    GameObject ringMove;
     GameObject ai;
-    int posIndex;
+    public int posIndex;
     MainAI mainAi;
     void Start()
     {
         ai = GameObject.Find("AI");
         mainAi = ai.GetComponent<MainAI>();
+
+        moveDummy.SetActive(false);
+        stageClearDummy.SetActive(false);
+
+        ringMove = GameObject.Find("RingMove");
+
         rayManager = GameObject.Find("RayManager").GetComponent<RayManager>();
+
         for (int i = 0; i < aiPoss.Length; i++)
         {
             aiPoss[i].SetActive(false);
@@ -32,6 +43,13 @@ public class AIPosManager : MonoBehaviour
 
         if (rayManager.hits.Length == 2)
         {
+            if (rayManager.hits[0].transform.gameObject == mainAi.rayTarget[1].gameObject ||
+                rayManager.hits[1].transform.gameObject == mainAi.rayTarget[1].gameObject)
+            {
+                moveDummy.SetActive(true);
+                ai.SetActive(false);
+            }
+
             if (rayManager.hits[0].transform.gameObject == rayTargets[posIndex].gameObject ||
                 rayManager.hits[1].transform.gameObject == rayTargets[posIndex].gameObject)
             {
@@ -54,6 +72,22 @@ public class AIPosManager : MonoBehaviour
                     aiPoss[aiPoss.Length - 1].SetActive(false);
                 }
             }
+        }
+
+        if (Vector3.Distance(moveDummy.transform.position, moveDestPos.position) < 1 &&
+            moveDummy.activeSelf == true)
+        {
+            ai.transform.position = moveDestPos.position;
+            ai.SetActive(true);
+            moveDummy.SetActive(false);
+        }
+
+        if (Vector3.Distance(ai.transform.position,
+            mainAi.wayPointBox[mainAi.wayPointBox.Length - 1].transform.position) < 1)
+        {
+            stageClearDummy.SetActive(true);
+            ai.SetActive(false);
+            ringMove.GetComponent<GroundMove>().enabled = true;
         }
     }
 }
---
+++ Assets/01.Scripts/02.MainGame/WayPointPasser.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointPasser : MonoBehaviour
+{
+    MainAI mainAi;
+    GameObject ai;
+    ObjActivity objAct;
+    AIPosManager aiPosMng;
+    void Start()
+    {
+        ai = GameObject.Find("AI");
+        mainAi = ai.GetComponent<MainAI>();
+        objAct = GameObject.Find("ObjActivity").GetComponent<ObjActivity>();
+        aiPosMng = GameObject.Find("AIPosManager").GetComponent<AIPosManager>();
+    }
+
+    void Update()
+    {
+        print(aiPosMng.posIndex);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (mainAi.wpIndex == 5 && mainAi.state == MainAI.AIState.Idle)
+            {
+                if (ai.activeSelf == true && aiPosMng.posIndex == 0)
+                {
+                    ai.SetActive(false);
+                }
+
+                if (aiPosMng.posIndex < aiPosMng.aiPoss.Length - 1)
+                {
+                    aiPosMng.aiPoss[aiPosMng.posIndex].SetActive(false);
+                    aiPosMng.rayTargets[aiPosMng.posIndex].SetActive(false);
+                    aiPosMng.posIndex++;
+                    aiPosMng.aiPoss[aiPosMng.posIndex].SetActive(true);
+                }
+                
+                if (aiPosMng.aiPoss[aiPosMng.aiPoss.Length - 1].activeSelf == true)
+                {
+                    ai.transform.position = aiPosMng.overbridgeDestPos.position;
+                    ai.SetActive(true);
+                    mainAi.wpIndex++;
+                    mainAi.state = MainAI.AIState.Run;
+                    aiPosMng.aiPoss[aiPosMng.aiPoss.Length - 1].SetActive(false);
+                }
+            }
+            
+            else if (mainAi.wpIndex == 3 && mainAi.state == MainAI.AIState.Idle && ai.activeSelf == true)
+            {
+                mainAi.wpIndex++;
+                // 토네이도 활성화
+                objAct.tornado.SetActive(true);
+                // AIDummy 활성화
+                objAct.torAiDummy.SetActive(true);
+                // AI 비활성화
+                ai.SetActive(false);
+                mainAi.state = MainAI.AIState.Run;
+                mainAi.anim.SetTrigger("Run");
+            }
+            
+            else if (mainAi.state == MainAI.AIState.Idle && ai.activeSelf == true)
+            {
+                mainAi.wpIndex++;
+                mainAi.state = MainAI.AIState.Run;
+                mainAi.anim.SetTrigger("Run");
+            }
+        }
+    }
+}
