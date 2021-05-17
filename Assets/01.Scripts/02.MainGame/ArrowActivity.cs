18ae996502e6ec711cd9ae793409464f90b8d707
SSo99r <ddvlep@gmail.com>
2021-05-17 10:23:55 +0900

    .

---
+++ Assets/01.Scripts/02.MainGame/ArrowActivity.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowActivity : MonoBehaviour
+{
+    public GameObject[] arrows;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
